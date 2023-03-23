1fbd7b703ecf4bab0b9700cffc95a32a95709cbf
Onome Blessing <onomeblessing4002@gmail.com>
2023-03-23 15:00:27 +0100

    I worked further on the css file

--- script.js
+++ script.js
@@ -23,7 +23,10 @@ for (let i = 0; i < navbarLinks.length; i++){
 }
 
 
+
+    // This is for the header section
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-tbn]");
 
 window.addEventListener("scroll", function() {
     if (window.scrollY >=100) {
@@ -31,10 +34,13 @@ window.addEventListener("scroll", function() {
     backTopBtn.classList.add("active");
     }else{
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 });
 
 
+
+    // This is for the search button
 const searchBtn = document.querySelector("[data-search-btn]");
 const searchContainer = document.querySelector("[data-search-container]");
 const searchSubmitBtn = document.querySelector("[data-search-submit-btn]");
@@ -50,7 +56,29 @@ for (let i = 0; 1 < searchBoxElems.length; i++) {
 }
 
 
+        // This is for the delivery boy
 
+const deliveryBoy = document.querySelector("[data-delivery-boy]");
+
+let deliveryBoyMove = -80;
+let lastScrollPos = 0;
+
+window.addEventListener("scroll", function () {
+    let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+
+    if(deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+        let activeScrollPos = window.scrollY;
+
+        if (lastScrollPos < activeScrollPos) {
+            deliveryBoyMove += 1;
+        }else{
+            deliveryBoyMove -= 1;
+        }
+
+        lastScrollPos = activeScrollPos;
+        deliveryBoy.style.transform = `translateX(${deliveryBoyMove})`;
+    }
+});
 
 
 
